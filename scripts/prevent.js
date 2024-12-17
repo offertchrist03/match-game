6e5b8c02c014c77b79323082d8cba7f5cdbfe243
Christ-Offert <142915490+offertchrist03@users.noreply.github.com>
2024-12-17 21:52:35 +1300

    Delete scripts directory

--- scripts/prevent.js
+++
@@ -1,8 +0,0 @@
-// NE PAS AUTORISER L'UTIILISATEUR A INSPECTER
-window.addEventListener("contextmenu", (e) => {
-  e.preventDefault();
-});
-
-window.addEventListener("keydown", (e) => {
-  e.preventDefault();
-});
